13c997d78490f13a052eab7047ae308f4452723b
NarendraReddy <nkadiri09@gmail.com>
2017-09-03 16:54:02 -0400

    Fetching the data with HQL. Query query = session.createQuery("from
    org.naren.kadiri.dto.UserDetails");
    List userList = query.list();

--- src/org/kadiri/hibernate/HibernateTest.java
+++ src/org/kadiri/hibernate/HibernateTest.java
@@ -1,16 +1,12 @@
 package org.kadiri.hibernate;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-import org.naren.kadiri.dto.Address;
-import org.naren.kadiri.dto.FourWheeler;
-import org.naren.kadiri.dto.TwoWheeler;
 import org.naren.kadiri.dto.UserDetails;
-import org.naren.kadiri.dto.Vehicle;
 
 public class HibernateTest {
 
@@ -19,18 +15,11 @@ public class HibernateTest {
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		UserDetails user = new UserDetails();
-		user.setUserName("Narendre");
-		session.save(user);
+		Query query = session.createQuery("from org.naren.kadiri.dto.UserDetails");
+
+		List userList = query.list();
 		session.getTransaction().commit();
 		session.close();
-
-		session = sessionFactory.openSession();
-		session.beginTransaction();
-		session.update(user);
-		session.getTransaction().commit();
-		session.close();
-
+		System.out.println("size of the table is: " + userList.size());
 	}
-
 }
